4ab8cea10bd5b63ed85e6e702966c50bbc31d153
KintonHiryuu <killianjilian@gmail.com>
2024-01-06 04:40:24 +0100

    () failed

--- chestStorage/wip.lua
+++ chestStorage/wip.lua
@@ -83,10 +83,11 @@ monitor.clear()
 monitor.setCursorPos(1,1)
 local x = 1
 table.sort(contentList)
- 
+
+print(math.log10(content[contentList[item]].count)+1)
 for item in pairs(contentList) do
     print(contentList[item])
-    monitor.write(("%s x %d nbr %d"):format(PadString(content[contentList[item]].name,maxLineSize-math.floor(math.log10(content[contentList[item]].count)+1)), content[contentList[item]].count), math.log10(content[contentList[item]].count)+1)
+    monitor.write(("%s x %d"):format(PadString(content[contentList[item]].name,maxLineSize-math.floor(math.log(content[contentList[item]].count)+1)), content[contentList[item]].count) )
     monitor.setCursorPos(1,x+1)
     x=x+1
 end
